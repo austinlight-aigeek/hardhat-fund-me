a8e35022a76def00e2bc1157f72c6feb88e2cee0
Austin Light <austinlight.fl@gmail.com>
2024-08-06 21:22:41 -0400

    helper-hardhat-config file added

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,4 +1,12 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+
+  const chainId = network.config.chainId;
+
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
 };
---
+++ helper-hardhat-config.js
@@ -0,0 +1,6 @@
+const networkConfig = {
+  11155111: {
+    name: "sepolia",
+    ethUsdPriceFeed: 0x694aa1769357215de4fac081bf1f309adc325306,
+  },
+};
