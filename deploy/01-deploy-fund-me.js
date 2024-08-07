e33011f122784406541160c2ecf2d2e8fe3bfba3
Austin Light <austinlight.fl@gmail.com>
2024-08-06 22:30:44 -0400

    deploy, verify contracts completed

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,12 +1,39 @@
+const { networkConfig } = require("../helper-hardhat-config");
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy, log } = deployments;
+  const { deploy, log, get } = deployments;
   const { deployer } = await getNamedAccounts();
 
   const chainId = network.config.chainId;
 
+  let ethUsdPriceFeedAddress;
+  if (chainId == 31337) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
+
+  const args = [ethUsdPriceFeedAddress];
+
   const fundMe = await deploy("FundMe", {
     from: deployer,
-    args: [],
+    args: [ethUsdPriceFeedAddress],
     log: true,
+    waitConfirmations: network.config.blockConfirmations,
   });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(fundMe.address, args);
+  }
+
+  log("------------------------------------------------");
 };
+
+module.exports.tags = ["all", "fundme"];
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,13 +4,28 @@ require("dotenv").config();
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.24",
-  network: {
+  networks: {
     sepolia: {
       url: SEPOLIA_RPC_URL,
-      chainId: 1115511,
+      chainId: 11155111,
+      accounts: [PRIVATE_KEY],
+      blockConfirmations: 6,
+    },
+    hardhat: {
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
   },
 };
